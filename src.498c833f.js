9bbdf81ab333a41d6397348ea1bc368e304c88f2
okolobaha-me <92094119+okolobaha-me@users.noreply.github.com>
2021-12-02 14:02:43 +0000

    Deploying to gh-pages from @ okolobaha-me/ooo-kudriyaviy-kod@d0c90faf7583aa73d6000b901ae0abfe06e9eb06 🚀

--- src.498c833f.js
+++ src.498c833f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ooo-kudriyaviy-kod/src.5aae5f99.js.map
+//# sourceMappingURL=/ooo-kudriyaviy-kod/src.498c833f.js.map
