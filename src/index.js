b2081c03acf6e01e56148fa001124049db98564f
fenota-viktoriya <Fen9206@gmail.com>
2021-12-03 14:20:33 +0200

    add swiper-slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+//инициализируем Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  
